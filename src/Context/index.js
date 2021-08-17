f501705b759353b863409c28ed639afd42f47d21
Eric Kreis <erickreis793@gmail.com>
2021-08-17 18:19:09 -0300

    Requisito 01 - Refatorando

--- src/Context/index.js
+++ src/Context/index.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
 export const MyContext = createContext();
@@ -6,10 +6,17 @@ export const MyContext = createContext();
 export default function MyProvider({ children }) {
   const [data, setData] = useState([]);
 
-  const contextValue = {
-    data,
-    setData,
-  };
+  useEffect(() => {
+    const getPlanets = async () => {
+      const PLANETS_URL = 'https://swapi-trybe.herokuapp.com/api/planets/';
+      const fetching = await fetch(PLANETS_URL);
+      const { results } = await fetching.json();
+      setData(results);
+    };
+    getPlanets();
+  }, []);
+
+  const contextValue = { data };
   return (
     <MyContext.Provider value={ contextValue }>
       { children }
--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,34 +1,21 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useContext } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { MyContext } from '../../Context';
 
 export default function Table() {
-  const [planets, setPlanets] = useState([]);
-  const { setData } = useContext(MyContext);
-
-  useEffect(() => {
-    const getPlanets = async () => {
-      const PLANETS_URL = 'https://swapi-trybe.herokuapp.com/api/planets/';
-      const fetching = await fetch(PLANETS_URL);
-      const { results } = await fetching.json();
-      setPlanets(results);
-    };
-    getPlanets();
-  }, []);
-
-  useEffect(() => { setData(planets); }, [planets, setData]);
+  const { data } = useContext(MyContext);
 
   return (
     <table>
       <thead>
         <tr>
-          { planets.length > 0
-          && Object.keys(planets[0])
+          { data.length > 0
+          && Object.keys(data[0])
             .map((key) => key !== 'residents' && <th key={ uuidv4() }>{ key }</th>) }
         </tr>
       </thead>
       <tbody>
-        { planets.map((planet) => (
+        { data.length > 0 && data.map((planet) => (
           <tr key={ uuidv4() }>
             { Object.keys(planet).map((key) => (
               key !== 'residents' && <td key={ uuidv4() }>{ planet[key] }</td>
