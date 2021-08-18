13d4cf579e39ef98e96d8626a766c8e6ae38aa09
Bruno Davi <brunodaviandrade2000@gmail.com>
2021-08-18 15:41:32 -0300

    Criando primeiro endpoint

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [residents, setResidents] = useState();
+
+  function getResidents() {
+    fetch('https://swapi-trybe.herokuapp.com/api/planets/')
+      .then((response) => response.json())
+      .then(({ results }) => setResidents(results.residents));
+  }
+
+  useEffect(getResidents);
+
   return (
-    <span>Hello, App!</span>
+    <table>
+      <tr>
+        <th>residents</th>
+      </tr>
+      <tr>
+        {residents.map((resident, i) => (
+          <td key={ i }>{resident}</td>
+        ))}
+      </tr>
+    </table>
   );
 }
 
