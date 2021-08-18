66c4c29c6536747b1698c29858976f83995cc406
rafaelDeoliveira <rafaelbatista02@gmail.com>
2021-08-18 14:26:49 -0300

    inicializando

---
+++ src/context/Mycontext.js
@@ -0,0 +1,28 @@
+import React, { createContext, useEffect, useState } from 'react';
+import { PropTypes } from 'prop-types';
+// import Api from './Api';
+
+const Context = createContext();
+
+function Provider({ children }) {
+  const [planets, setPlanets] = useState([]);
+  // const [isLoand, setSloand] = useState(false);
+
+  useEffect(() => {
+    const getApi = async () => {
+      const callApi = await Api();
+      setPlanets(callApi);
+    };
+    getApi();
+  }, []);
+
+  return (
+    <Context.Provider value={ { planets } }>
+      {children}
+    </Context.Provider>);
+}
+
+Provider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+export { Context, Provider };
