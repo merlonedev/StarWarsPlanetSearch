433ab67ea4464c647bd5ca6613ab52ec0b47f375
Rodrigo <rodrigo.facury14@gmail.com>
2021-08-18 20:13:54 -0300

    Parte do requisito 3

--- src/App.js
+++ src/App.js
@@ -6,12 +6,18 @@ import MyContext from './Context/MyContext';
 function App() {
   const [data, setData] = useState([]);
   const [name, setName] = useState('');
+  const [numberState, setNumberState] = useState({});
+  const [numberFilter, setNumberFilter] = useState({});
+
   const contextValue = {
     data,
     filters: {
       filterByName: {
         name,
       },
+      filterByNumericValues: [
+        numberFilter,
+      ],
     },
   };
 
@@ -32,7 +38,28 @@ function App() {
   return (
     <MyContext.Provider value={ contextValue }>
       <main>
-        <input type="text" data-testid="name-filter" onChange={ handleNameChange } />
+        <form>
+          <input
+            type="text"
+            data-testid="name-filter"
+            onChange={ handleNameChange }
+            placeholder="Filtrar pelo nome"
+          />
+          <select data-testid="column-filter" name="column">
+            <option value="population">population</option>
+            <option value="orbital_period">orbital_period</option>
+            <option value="diameter">diameter</option>
+            <option value="rotation_period">rotation_period</option>
+            <option value="surface_water">surface_water</option>
+          </select>
+          <select data-testid="comparison-filter" name="comparison">
+            <option value="maior que">maior que</option>
+            <option value="menor que">menor que</option>
+            <option value="igual a">igual a</option>
+          </select>
+          <input type="number" data-testid="button-filter" name="value" />
+          <button type="button" data-testid="button-filter">Filtrar</button>
+        </form>
         { (contextValue.data.length > 0) && <Table /> }
       </main>
     </MyContext.Provider>
