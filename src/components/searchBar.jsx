ae3973f6d763c26c5b3101ea49c2e87e8a4415f6
pedro21medeiros <pedro21medeiros@gmail.com>
2021-08-18 15:45:27 -0300

    Cria a search bar para filtros

---
+++ src/components/searchBar.jsx
@@ -0,0 +1,67 @@
+import React, { useContext, useState } from 'react';
+import MyContext from '../context/MyContext';
+
+function SearchBar() {
+  const { filters: { filterByName: { name }, numericFilter },
+    setNameFilter, setFilters } = useContext(MyContext);
+
+  const initialColumnOpt = ['population',
+    'orbital_period', 'diameter', 'rotation_period', 'surface_water'];
+  const [column, setColumn] = useState(initialColumnOpt);
+  const [comparison, setComparison] = useState('');
+  const [value, setValue] = useState(0);
+
+  const columnOptions = !column ? initialColumnOpt
+    : initialColumnOpt.filter((resp) => resp !== column);
+
+  const handleFilterButton = () => {
+    const filterState = {
+      column,
+      comparison,
+      value,
+    };
+    setFilters([...numericFilter, filterState]);
+  };
+
+  return (
+    <>
+      <input
+        type="text"
+        data-testid="name-filter"
+        value={ name }
+        onChange={ ({ target }) => setNameFilter(target.value) }
+      />
+      <select
+        data-testid="column-filter"
+        onChange={ ({ target }) => setColumn(target.value) }
+      >
+        {columnOptions.map((option, index) => (
+          <option key={ index } value={ option }>{option}</option>
+        ))}
+      </select>
+      <select
+        data-testid="comparison-filter"
+        onChange={ ({ target }) => setComparison(target.value) }
+      >
+        <option value="maior que">maior que</option>
+        <option value="menor que">menor que</option>
+        <option value="igual a">igual a</option>
+      </select>
+      <input
+        type="number"
+        data-testid="value-filter"
+        value={ value }
+        onChange={ ({ target }) => setValue(Number(target.value)) }
+      />
+      <button
+        type="button"
+        data-testid="button-filter"
+        onClick={ handleFilterButton }
+      >
+        Filtrar
+      </button>
+    </>
+  );
+}
+
+export default SearchBar;
