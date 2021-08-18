67c137286b2811a23c5ce25a4244a5e0159fa723
Rodrigo <rodrigo.facury14@gmail.com>
2021-08-18 17:08:55 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Table from './Components/Table';
+import MyContext from './Context/MyContext';
 
 function App() {
+  const [data, setData] = useState([]);
+  const contextValue = {
+    data,
+  };
+
+  useEffect(() => {
+    const getPlanets = async () => {
+      const endpoint = 'https://swapi-trybe.herokuapp.com/api/planets/';
+      const { results } = await fetch(endpoint).then((planets) => planets.json());
+      setData(results);
+    };
+
+    getPlanets();
+  }, []);
+
   return (
-    <span>Hello, App! :D</span>
+    <MyContext.Provider value={ contextValue }>
+      { (contextValue.data.length > 0) && <Table /> }
+    </MyContext.Provider>
   );
 }
 
