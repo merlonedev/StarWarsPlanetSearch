f1042ec4067aad0004bd3505ed5120c60feafc2c
Kevin Oliveira <kevin.zero@hotmail.com>
2021-08-18 18:28:36 -0300

    4 - Remove opcao de filtro apos ser aplicado

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,25 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import FilterContext from '../context/FilterContext';
 
 function Header() {
-  const { handleChangeName, handleFilter } = useContext(FilterContext);
-  const [column, setColumn] = useState('population');
-  const [comparison, setComparison] = useState('maior que');
-  const [number, setNumber] = useState('');
-
   const columnOptions = ['population', 'orbital_period', 'diameter',
     'rotation_period', 'surface_water'];
   const comparisonOptions = ['maior que', 'menor que', 'igual a'];
 
-  const handleClick = () => handleFilter({ column, comparison, value: number });
+  const { handleChangeName, handleFilter } = useContext(FilterContext);
+  const [column, setColumn] = useState('population');
+  const [comparison, setComparison] = useState('maior que');
+  const [number, setNumber] = useState('');
+  const [uniqueColumns, setUniqueColumns] = useState(columnOptions);
+
+  const handleClick = () => {
+    if (number) {
+      handleFilter({ column, comparison, value: number });
+      setUniqueColumns(...[uniqueColumns.filter((item) => item !== column)]);
+    }
+  };
+
+  useEffect(() => setColumn(uniqueColumns[0]), [uniqueColumns]);
 
   return (
     <header>
@@ -23,9 +31,10 @@ function Header() {
       />
       <select
         data-testid="column-filter"
+        value={ column }
         onChange={ ({ target: { value } }) => setColumn(value) }
       >
-        {columnOptions.map((option) => (
+        {uniqueColumns.map((option) => (
           <option value={ option } key={ option }>{option}</option>
         ))}
       </select>
