b9260de25e0323c2ad56da04eebac25d4b617c6f
alestori <alestori7@gmail.com>
2021-08-18 16:27:22 -0300

    mostra os nomes dos filmes na coluna films

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -15,6 +15,15 @@ function Table() {
   if (name) planets = planets.filter((d) => d.name.toLowerCase().includes(name));
   if (!planets.length) return 'No Planets Found';
 
+  const swFilms = {
+    1: 'A New Hope',
+    2: 'The Empire Strikes Back',
+    3: 'The Return of Jedi',
+    4: 'The Phantom Menace',
+    5: 'The Attack of the Clones',
+    6: 'The Revenge of the Sith',
+  };
+
   return (
     <table>
       <thead>
@@ -27,9 +36,25 @@ function Table() {
       <tbody>
         {planets.map((planet, i) => (
           <tr key={ i }>
-            {Object.values(planet).map((value, key) => (
-              <td key={ key }>{value}</td>
-            ))}
+            {Object.values(planet).map((value, key) => {
+              if (Array.isArray(value)) {
+                const movieNames = Object.values(value)
+                  .map((film) => swFilms[film
+                    .split('/')[(film.split('/')).length - 2]])
+                  .join(', ');
+
+                return (
+                  <td key={ `cell-${key}` }>
+                    <span>
+                      {movieNames}
+                    </span>
+                  </td>
+                );
+              }
+              return (
+                <td key={ key }>{value}</td>
+              );
+            })}
           </tr>
         ))}
       </tbody>
