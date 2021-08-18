d1b7c7685f14700f425c0ca0c4e298d80b4daf01
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-08-18 15:20:15 -0300

    criando componente table

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Provider from './context/Provider';
+import Table from './components/Table';
 import './App.css';
 
 function App() {
   return (
     <Provider>
-      <span>Iniciando projeto!</span>
+      <Table />
     </Provider>
   );
 }
---
+++ src/components/Table.js
@@ -0,0 +1,34 @@
+import React, { useContext } from 'react';
+import StarWarsContext from '../context/StarWarsContext';
+
+function Table() {
+  const { data } = useContext(StarWarsContext);
+
+  const renderTableRow = (data) => {
+    return (
+      <tr key={data.name}>
+        {Object.values(data).map((value) => <td key={value}>{value}</td>)}
+      </tr>
+    );
+  }
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          {data[0] && Object.keys(data[0]).map((title) => {
+            let newTitle = title.split('_').join(' ');
+            newTitle = newTitle[0].toUpperCase() + newTitle.substr(1);
+            if (title === 'residents') return null;
+            return <th key={ title }>{ newTitle }</th>;
+          })}
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((planet) => renderTableRow(planet))}
+      </tbody>
+    </table>
+  );
+}
+
+export default Table;
