77dc9ad66eaca5c36c1aa348ba187be1a761226f
tomsantos07 <xotmsantosx@live.com>
2021-08-18 15:32:51 -0300

    construçao da requisiçao na Api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    const getPlanets = async () => {
+      const endpoint = 'https://swapi-trybe.herokuapp.com/api/planets/';
+      const result = await fetch(endpoint).then((response) => response.json());
+      console.log(result);
+    };
+    getPlanets();
+  }, []);
   return (
     <span>Hello, App!</span>
   );
