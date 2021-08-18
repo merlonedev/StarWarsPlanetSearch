7d6dc1797a510a17458fc12647420b3c262e1478
Thalles Carneiro <thallescarneiro@gmail.com>
2021-08-18 19:15:19 -0300

    Implementa componente para mostrar na página os filtros adicionados e um botão possibilitando removê-los

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import StarWarsProvider from './context/StarWarsProvider';
 import FilterForm from './components/FilterForm';
+import FilterByNumericValue from './components/FilterByNumericValue';
 import Table from './components/Table';
 import './App.css';
 
@@ -8,6 +9,7 @@ const App = () => (
   <section>
     <StarWarsProvider>
       <FilterForm />
+      <FilterByNumericValue />
       <Table />
     </StarWarsProvider>
   </section>
---
+++ src/components/FilterByNumericValue/index.js
@@ -0,0 +1,34 @@
+import React, { useContext } from 'react';
+import StarWarsContext from '../../context/StarWarsContext';
+
+const FilterByNumericValue = () => {
+  const { filters, setFilters } = useContext(StarWarsContext);
+  const { filterByNumericValues } = filters;
+
+  const handleRemoveFilter = (filter) => {
+    const newComparisonFilter = filterByNumericValues
+      .filter(({ column }) => column !== filter.column);
+    setFilters({
+      ...filters,
+      filterByNumericValues: newComparisonFilter,
+    });
+  };
+
+  return (
+    <ul>
+      { filterByNumericValues.map((filter) => (
+        <li key={ filter.column } data-testid="filter">
+          <p>{ JSON.stringify(filter) }</p>
+          <button
+            type="button"
+            onClick={ () => handleRemoveFilter(filter) }
+          >
+            X
+          </button>
+        </li>
+      )) }
+    </ul>
+  );
+};
+
+export default FilterByNumericValue;
