e688c3df10e1570737f2e032bd494deb0f7dc62b
Matheus Proença <mproenca.marketing@gmail.com>
2021-08-18 18:35:09 -0300

    criando repositorio de components e context

---
+++ src/context/Provider.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import AppContext from './AppContext';
+
+function Provider({ children }) {
+  return (
+    <AppContext.Provider>
+      { children}
+    </AppContext.Provider>
+  );
+}
+
+Provider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Provider;
