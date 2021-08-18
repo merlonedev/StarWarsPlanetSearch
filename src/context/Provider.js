1ff60260ac3ea87c05da9a7b4d2756889806e071
pedro21medeiros <pedro21medeiros@gmail.com>
2021-08-18 14:59:35 -0300

    Cria o Provider da aplicaçao

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import MyContext from './MyContext';
 
 function Provider({ children }) {
-  const [plantes, setPlanets] = useState([]);
+  const [planets, setPlanets] = useState([]);
   const [name, setNameFilter] = useState('');
   const [numericFilter, setFilters] = useState([]);
 
@@ -10,8 +11,34 @@ function Provider({ children }) {
     const getPlanets = async () => {
       const endpoint = 'https://swapi-trybe.herokuapp.com/api/planets/';
       const { results } = await fetch(endpoint).then((data) => data.json());
+      setPlanets(results.filters((resp) => delete resp.residents));
     };
     getPlanets();
   }, []);
+  const contextValue = {
+    planets,
+    filters: {
+      filterByName: {
+        name,
+      },
+      numericFilter,
+    },
+    setNameFilter,
+    setFilters,
+  };
+
+  return (
+    <MyContext.Provider value={ contextValue }>
+      {children}
+    </MyContext.Provider>
+  );
 }
 
+Provider.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]).isRequired,
+};
+
+export default Provider;
