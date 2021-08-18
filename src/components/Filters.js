4e99eefea60f79655af4a18e5fb17b65f3c07324
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-08-18 16:13:13 -0300

    criando metodo para renderizar as tags pros filtros

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,6 +1,14 @@
 import React, { useContext } from 'react';
 import StarWarsContext from '../context/StarWarsContext';
 
+const selectValues = {
+  population: 'Population',
+  orbital_period: 'Orbital period',
+  diameter: 'Diameter',
+  rotation_period: 'Rotation period',
+  surface_water: 'Surface water',
+};
+
 function Filters() {
   const { filters, setFilter } = useContext(StarWarsContext);
   const { filterByName: { name } } = filters;
@@ -20,9 +28,39 @@ function Filters() {
     </label>
   );
 
+  const renderNumericFilters = () => (
+    <div>
+      <select id="column" data-testid="column-filter">
+        { Object.keys(selectValues).map((value) => (
+          <option key={ value } value={ value }>{ selectValues[value]}</option>
+        ))}
+      </select>
+      <select id="comparison" data-testid="comparison-filter">
+        <option value="maior que">Maior que</option>
+        <option value="menor que">Menor que</option>
+        <option value="igual a">Igual a</option>
+      </select>
+      <input
+        type="number"
+        value=""
+        id="value"
+        data-testid="comparison-filter"
+        onChange={ () => {} }
+      />
+      <button
+        type="button"
+        data-testid="button-filter"
+      >
+        Adicionar Filtro
+      </button>
+    </div>
+  );
+    
+
   return (
     <div>
       {renderPlanetInput()}
+      {renderNumericFilters()}
     </div>
   );
 }
