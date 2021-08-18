a38eadecb909ad7ac45489ae1e0f738ad4d3247c
Djaniza Vasques <djanizav@gmail.com>
2021-08-18 03:35:05 -0300

    Inicia o projeto, organiza pastas e adiciona header

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>Star Wars Planets</h1>
+      <h3> Search Engine</h3>
+    </div>
+  );
+}
+
+export default Header;
