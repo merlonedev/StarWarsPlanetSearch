17765acd3bc9b1a88dceb7ef0336c06c34d0a24d
alestori <alestori7@gmail.com>
2021-08-18 01:52:29 -0300

    cards de filtros

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AppContext from '../context/AppContext';
 import Filters from './filters/Filters';
 
 function Header() {
+  const { filters: { filterByNumericValues } } = useContext(AppContext);
+
+  const filterCards = () => {
+    const columns = {
+      'maior que': '>',
+      'menor que': '<',
+      'igual a': '=',
+    };
+
+    return filterByNumericValues.map(({ column, comparison, value }) => (
+      <div key={ `${column}${comparison}${value}` } className="filter-card">
+        <p>
+          {column}
+          {' '}
+          {columns[comparison]}
+          {' '}
+          {value}
+        </p>
+      </div>
+    ));
+  };
   return (
     <header className="page-header">
       <div className="title-div">
@@ -9,6 +31,9 @@ function Header() {
         <h3 className="title">Planet Search</h3>
       </div>
       <Filters />
+      <div className="filter-cards">
+        {filterCards()}
+      </div>
     </header>
   );
 }
