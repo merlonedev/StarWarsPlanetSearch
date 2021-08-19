4671a251b79cbc2de0e8b54ce364df1deaf1da8e
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-08-18 22:28:11 -0300

    implementa component TablePlanets e conclui requisito_01 - Faça uma requisição para API de Star Wars e preencha uma tabela

--- src/components/TablePlanets.js
+++ src/components/TablePlanets.js
@@ -2,11 +2,45 @@ import React, { useContext } from 'react';
 import Context from '../context/Context';
 
 function TabletPlanets() {
-  const { planets } = useContext(Context);
-  const HEADERS_TABLE = Object.keys(planets[0]);
-  console.log(HEADERS_TABLE);
+  const { newListPlanets } = useContext(Context);
+
+  const HEADERS_TABLE = Object.keys(newListPlanets[0]);
+
+  const NUMBER_RAMDOM = 10000;
+
+  const LineHeader = (
+    <tr>
+      {
+        HEADERS_TABLE.map((header) => (
+          <th key={ Math.random() * NUMBER_RAMDOM }>
+            { header.replace('_', ' ').toUpperCase() }
+          </th>
+        ))
+      }
+    </tr>
+  );
+
+  const linesContent = (
+    newListPlanets.map((planet) => (
+      <tr key={ Math.random() * NUMBER_RAMDOM }>
+        {
+          Object.values(planet).map((type) => (
+            <td key={ Math.random() * NUMBER_RAMDOM }>{ type }</td>
+          ))
+        }
+      </tr>
+    ))
+  );
+
   return (
-    <p>TabletPlanets online</p>
+    <table>
+      <thead>
+        { LineHeader }
+      </thead>
+      <tbody>
+        { linesContent }
+      </tbody>
+    </table>
   );
 }
 
