84ebb315d3d1123a9c8ff49a5db2852456a3b33f
Euller Braz <euller.braz99@gmail.com>
2021-08-19 19:50:30 -0300

    Refatorado parte do código, adicionado componentização

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Input = ({ inputType, placeholder, value, onChange, name, id, testId }) => (
-  <input
-    type={ inputType }
-    placeholder={ placeholder }
-    value={ value }
-    onChange={ onChange }
-    name={ name }
-    id={ id }
-    data-testid={ testId }
-  />
+const Input = ({
+  inputType, placeholder, value, onChange, name, id, checked, labelText, testId,
+}) => (
+  <div>
+    <label htmlFor={ id }>
+      { labelText }
+    </label>
+    <input
+      type={ inputType }
+      placeholder={ placeholder }
+      value={ value }
+      onChange={ onChange }
+      name={ name }
+      id={ id }
+      checked={ checked }
+      data-testid={ testId }
+    />
+  </div>
 );
 
 Input.propTypes = {
@@ -23,12 +31,16 @@ Input.propTypes = {
   onChange: PropTypes.func.isRequired,
   name: PropTypes.string.isRequired,
   id: PropTypes.string,
+  checked: PropTypes.bool,
+  labelText: PropTypes.string,
   testId: PropTypes.string.isRequired,
 };
 
 Input.defaultProps = {
   placeholder: '',
   id: '',
+  checked: false,
+  labelText: '',
 };
 
 export default Input;
