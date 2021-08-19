ca83635dfb5f7cd705df0b46acc905755f412d47
MiguelRetroz <contato.miguelretroz@gmail.com>
2021-08-19 01:45:19 -0300

    Requirement 03-> Add input fields for numeric filters

--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -1,24 +1,104 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import AppContext from '../context/AppContext';
 
 function FilterBar() {
   const { filters, setFilters } = useContext(AppContext);
+  const columnFilterList = [
+    'population',
+    'orbital_period',
+    'diameter',
+    'rotation_period',
+    'surface_water',
+  ];
+  const comparisonFilterList = ['maior que', 'menor que', 'igual a'];
+  const [inputFilters, setInputFilters] = useState({
+    column: 'population',
+    comparison: 'maior que',
+    value: 0,
+  });
 
   const handleChange = ({ target: { name, value } }) => {
-    if (name === 'name') setFilters({ ...filters, filterByName: { name: value } });
+    if (name === 'name') {
+      setFilters({ ...filters, filterByName: { name: value } });
+    } else {
+      const correctValue = (name === 'value' && value < 0) ? 0 : value;
+      setInputFilters({
+        ...inputFilters,
+        [name]: correctValue,
+      });
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setFilters({
+      ...filters,
+      filterByNumericValues: [...filters.filterByNumericValues, inputFilters],
+    });
   };
 
   return (
-    <label htmlFor="name-filter">
-      Nome
-      <input
-        data-testid="name-filter"
-        id="name-filter"
-        name="name"
-        onChange={ handleChange }
-        type="text"
-      />
-    </label>
+    <>
+      <label htmlFor="name-filter">
+        Nome
+        <input
+          data-testid="name-filter"
+          id="name-filter"
+          name="name"
+          onChange={ handleChange }
+          type="text"
+          value={ filters.filterByName.name }
+        />
+      </label>
+      <form onSubmit={ handleSubmit }>
+        <label htmlFor="column-filter">
+          Coluna
+          <select
+            id="column-filter"
+            data-testid="column-filter"
+            name="column"
+            onChange={ handleChange }
+            required
+            value={ inputFilters.column }
+          >
+            { columnFilterList
+              .map((column) => (
+                <option key={ column } value={ column }>{ column }</option>
+              )) }
+          </select>
+        </label>
+        <label htmlFor="comparison-filter">
+          Comparação
+          <select
+            id="comparison-filter"
+            data-testid="comparison-filter"
+            name="comparison"
+            onChange={ handleChange }
+            required
+            value={ inputFilters.comparison }
+          >
+            { comparisonFilterList
+              .map((comparison) => (
+                <option key={ comparison } value={ comparison }>{ comparison }</option>
+              )) }
+          </select>
+        </label>
+        <label htmlFor="value-filter">
+          Valor
+          <input
+            data-testid="value-filter"
+            id="value-filter"
+            name="value"
+            onChange={ handleChange }
+            required
+            type="number"
+            value={ inputFilters.value }
+          />
+        </label>
+        <button type="submit" data-testid="button-filter">Adicionar filtro</button>
+      </form>
+    </>
   );
 }
 
