f227436df5b0f242d47b57c0b389fe2b731e24dd
Roberval Filho <rslfilho@gmail.com>
2021-08-19 10:27:10 -0300

    Requisito 05

---
+++ src/components/Filters/UsedFilters.js
@@ -0,0 +1,71 @@
+import React, { useContext } from 'react';
+import Context from '../../context/Context';
+import { columns, comparisonMethods } from '../../utils/data';
+
+const UsedFilters = () => {
+  const {
+    setFilterByNumericValue,
+    filters: { filterByNumericValue },
+  } = useContext(Context);
+
+  const renderFilterColumn = (type) => {
+    switch (type) {
+    case columns[0]:
+      return 'Population ';
+    case columns[1]:
+      return 'Orbital Period ';
+    case columns[2]:
+      return 'Diameter ';
+    case columns[3]:
+      return 'Rotation Period ';
+    case columns[4]:
+      return 'Surface Water ';
+    default:
+      return null;
+    }
+  };
+
+  const renderComparisonMethod = (type) => {
+    switch (type) {
+    case comparisonMethods[0]:
+      return 'bigger then ';
+    case comparisonMethods[1]:
+      return 'less than ';
+    case comparisonMethods[2]:
+      return 'equal to';
+    default:
+      return null;
+    }
+  };
+
+  const renderDeleteIcon = (index) => (
+    <button
+      type="button"
+      onClick={ () => {
+        setFilterByNumericValue(filterByNumericValue
+          .filter((_filter, i) => i !== index));
+      } }
+    >
+      ❌
+    </button>
+  );
+
+  return (
+    <div>
+      <ul>
+        {
+          filterByNumericValue.map(({ column, comparison, value }, index) => (
+            <li key={ `filter-${index}` } data-testid="filter">
+              {renderFilterColumn(column)}
+              {renderComparisonMethod(comparison)}
+              {`${value}  `}
+              { renderDeleteIcon(index) }
+            </li>
+          ))
+        }
+      </ul>
+    </div>
+  );
+};
+
+export default UsedFilters;
--- src/components/Filters/index.js
+++ src/components/Filters/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import Context from '../../context/Context';
 import FilterByName from './FilterByName';
 import FilterByNumericValues from './FilterByNumericValues';
+import UsedFilters from './UsedFilters';
 
-const Filters = () => (
-  <div>
-    <FilterByName />
-    <FilterByNumericValues />
-  </div>
-);
+const Filters = () => {
+  const { filters: { filterByNumericValue } } = useContext(Context);
+
+  return (
+    <div>
+      <FilterByName />
+      <FilterByNumericValues />
+      { filterByNumericValue.length > 0 && <UsedFilters />}
+    </div>
+  );
+};
 
 export default Filters;
