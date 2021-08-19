0038824e537aca11ffb9fbb6135a506a162f9247
Sérgio A. Barbosa <sergio.alvesbarbosa@gmail.com>
2021-08-19 17:53:33 -0300

    Resolve requisito 02

--- src/pages/CreateTable.js
+++ src/pages/CreateTable.js
@@ -1,56 +1,93 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import MyContext from '../context/MyContext';
 
 function CreateTable() {
   const data = useContext(MyContext);
-  console.log(data);
+  const [filter, setfilter] = useState({
+    filterByName: {
+      name: '',
+    },
+  });
+
+  const [filtered, setfiltered] = useState([]);
+
+  useEffect(() => {
+    function filterDataByInput() {
+      let newArray = data;
+      if (newArray.length > 0 && filter.filterByName.name) {
+        newArray = newArray
+          .filter((item) => item.name.toLowerCase()
+            .includes(filter.filterByName.name.toLowerCase()));
+      }
+      setfiltered(newArray);
+    }
+    filterDataByInput();
+  },
+  [data, filter]);
+
+  const handleInputCharacter = ({ target: { value } }) => {
+    setfilter({
+      ...filter,
+      filterByName: {
+        name: value,
+      },
+    });
+  };
+
   return (
-    <table>
-      <tbody>
-        <tr>
-          <th>Planet</th>
-          <th>Rotation Period</th>
-          <th>Orbital Period</th>
-          <th>Diameter</th>
-          <th>Climate</th>
-          <th>Gravity</th>
-          <th>Terrain</th>
-          <th>Surface water</th>
-          <th>Population</th>
-          <th>Films</th>
-          <th>Created</th>
-          <th>Edited</th>
-          <th>URL</th>
-        </tr>
-        {
-          data.map((planet, index) => (
-            <tr key={ index }>
-              <td>{ planet.name }</td>
-              <td>{ planet.rotation_period }</td>
-              <td>{ planet.orbital_period }</td>
-              <td>{ planet.diameter }</td>
-              <td>{ planet.climate }</td>
-              <td>{ planet.gravity }</td>
-              <td>{ planet.terrain }</td>
-              <td>{ planet.surface_water }</td>
-              <td>{ planet.population }</td>
-              <td>{ planet.films }</td>
-              <td>{ planet.created }</td>
-              <td>{ planet.edited }</td>
-              <td>
-                <a
-                  href={ planet.url }
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  { planet.url }
-                </a>
-              </td>
-            </tr>
-          ))
-        }
-      </tbody>
-    </table>
+    <>
+      <input
+        type="text"
+        onChange={ (e) => handleInputCharacter(e) }
+        data-testid="name-filter"
+      />
+      <table>
+        <tbody>
+          <tr>
+            <th>Planet</th>
+            <th>Rotation Period</th>
+            <th>Orbital Period</th>
+            <th>Diameter</th>
+            <th>Climate</th>
+            <th>Gravity</th>
+            <th>Terrain</th>
+            <th>Surface water</th>
+            <th>Population</th>
+            <th>Films</th>
+            <th>Created</th>
+            <th>Edited</th>
+            <th>URL</th>
+          </tr>
+          {
+            filtered.map((planet, index) => (
+              <tr key={ index }>
+                <td>{ planet.name }</td>
+                <td>{ planet.rotation_period }</td>
+                <td>{ planet.orbital_period }</td>
+                <td>{ planet.diameter }</td>
+                <td>{ planet.climate }</td>
+                <td>{ planet.gravity }</td>
+                <td>{ planet.terrain }</td>
+                <td>{ planet.surface_water }</td>
+                <td>{ planet.population }</td>
+                <td>{ planet.films.map((film, i) => (<p key={ i }>{ film }</p>)) }</td>
+                <td>{ planet.created }</td>
+                <td>{ planet.edited }</td>
+                <td>
+                  <a
+                    href={ planet.url }
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    { planet.url }
+                  </a>
+                </td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+    </>
   );
 }
 
