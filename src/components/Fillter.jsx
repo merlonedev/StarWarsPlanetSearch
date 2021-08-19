cb1ddfc934ee6770fa2d24cb4d2ec149ca96715a
aPaulinhaCarlos <apaulinhacarlos@gmail.com>
2021-08-19 19:46:05 -0300

    Inicia a resolucao do requisito 5

--- src/components/Fillter.jsx
+++ src/components/Fillter.jsx
@@ -1,11 +1,23 @@
-// import React, { useContext } from 'react';
-// import PlanetContext from '../context/PlanetContext';
-import React from 'react';
+import React, { useContext } from 'react';
+import PlanetContext from '../context/PlanetContext';
 
 function Filter() {
+  const { filters: { filterByNumericValues } } = useContext(PlanetContext);
+  console.log(filterByNumericValues);
+
+  const exibir = () => {
+    const { column, comparison, value } = filterByNumericValues;
+    console.log(column);
+
+    if (column || comparison || value) {
+      return (<p>{ `${column} | ${comparison} | ${value}` }</p>);
+    }
+  };
+
   return (
     <div className="App">
       <h2>Eu sou o Componente Filtro!</h2>
+      { exibir() }
     </div>
   );
 }
