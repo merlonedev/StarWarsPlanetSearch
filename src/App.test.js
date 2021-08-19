f4484fe7a35e3a4fc9843be8a30334a9d7fb5aae
Roberval Filho <rslfilho@gmail.com>
2021-08-19 15:10:36 -0300

    Mudando package.json e reinstalando dependências

--- src/App.test.js
+++ src/App.test.js
@@ -54,7 +54,7 @@ describe('1 - Faça uma requisição para o endpoint `/planets` da API de Star W
       const terrain = await screen.findAllByText(planets[planetIndex].terrain);
       const surfaceWater = await screen.findAllByText(planets[planetIndex].surface_water);
       const population = await screen.findAllByText(planets[planetIndex].population);
-      
+
       expect(name).toBeInTheDocument();
       expect(rotationPeriod.length).toBeGreaterThanOrEqual(1);
       expect(orbitalPeriod.length).toBeGreaterThanOrEqual(1);
