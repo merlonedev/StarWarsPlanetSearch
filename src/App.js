55e572caff579902c80101ad81017fc20d9ed50f
Rodrigo <rodrigo.facury14@gmail.com>
2021-08-19 17:30:27 -0300

    Requisito 3

--- src/App.js
+++ src/App.js
@@ -35,6 +35,19 @@ function App() {
     setName(target.value);
   };
 
+  const handleNumbersChange = ({ target }) => {
+    const { name: tag, value } = target;
+
+    setNumberState({
+      ...numberState,
+      [tag]: value,
+    });
+  };
+
+  const handleSendFilter = () => {
+    setNumberFilter(numberState);
+  };
+
   return (
     <MyContext.Provider value={ contextValue }>
       <main>
@@ -45,20 +58,39 @@ function App() {
             onChange={ handleNameChange }
             placeholder="Filtrar pelo nome"
           />
-          <select data-testid="column-filter" name="column">
+          <select
+            data-testid="column-filter"
+            name="column"
+            onChange={ handleNumbersChange }
+          >
             <option value="population">population</option>
             <option value="orbital_period">orbital_period</option>
-            <option value="diameter">diameter</option>
             <option value="rotation_period">rotation_period</option>
+            <option value="diameter">diameter</option>
             <option value="surface_water">surface_water</option>
           </select>
-          <select data-testid="comparison-filter" name="comparison">
+          <select
+            data-testid="comparison-filter"
+            name="comparison"
+            onChange={ handleNumbersChange }
+          >
             <option value="maior que">maior que</option>
             <option value="menor que">menor que</option>
             <option value="igual a">igual a</option>
           </select>
-          <input type="number" data-testid="button-filter" name="value" />
-          <button type="button" data-testid="button-filter">Filtrar</button>
+          <input
+            type="number"
+            data-testid="value-filter"
+            name="value"
+            onChange={ handleNumbersChange }
+          />
+          <button
+            type="button"
+            data-testid="button-filter"
+            onClick={ handleSendFilter }
+          >
+            Filtrar
+          </button>
         </form>
         { (contextValue.data.length > 0) && <Table /> }
       </main>
--- src/Components/Table.js
+++ src/Components/Table.js
@@ -4,11 +4,32 @@ import MyContext from '../Context/MyContext';
 function Table() {
   const { data, filters } = useContext(MyContext);
   const { filterByName: { name } } = filters;
+  const { filterByNumericValues: [{ column, comparison, value }] } = filters;
   const nameLowerCase = name.toLowerCase();
-  const filteredData = data.filter((planet) => planet.name.toLowerCase()
+  const dataFilteredByName = data.filter((planet) => planet.name.toLowerCase()
     .includes(nameLowerCase));
   const tableHeads = Object.keys(data[0]);
   const filteredHeads = tableHeads.filter((head) => head !== 'residents');
+
+  const compare = (whichColumn, comparisonType, number, array) => {
+    switch (comparisonType) {
+    case 'maior que':
+      return array.filter((information) => Number(information[whichColumn]) > number);
+    case 'menor que':
+      return array.filter((information) => Number(information[whichColumn]) < number);
+    case 'igual a':
+      return array.filter((information) => information[whichColumn] === number);
+    default:
+      return array;
+    }
+  };
+
+  // Utilização do NumberConstructor feita com base na dica da colega Adriana Biberg
+
+  const filteredData = compare(column, comparison, value, dataFilteredByName);
+
+  // console.log(filteredData);
+  // console.log(column, comparison, value);
 
   return (
     <table>
