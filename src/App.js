d7b67fdc58cb6404c6e88abd78ae63dfcd96f285
Matheus Proença <mproenca.marketing@gmail.com>
2021-08-18 22:40:59 -0300

    Adiciona req01

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Table from './components/Table';
+import AppContext from './context/AppContext';
 
 function App() {
+  const [planets, setPlanets] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const END_POINT = 'https://swapi-trybe.herokuapp.com/api/planets/';
+      try {
+        const { results } = await fetch(END_POINT).then((data) => data.json());
+        results.forEach((planet) => delete planet.residents);
+        setPlanets(results);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getData();
+  }, []);
+
+  const contextValue = {
+    planets,
+  };
+
   return (
-    <span>Hello, App!</span>
+    <AppContext.Provider value={ contextValue }>
+      <Table />
+    </AppContext.Provider>
   );
 }
 
