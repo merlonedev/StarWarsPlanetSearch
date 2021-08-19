aafd5ee0dccf904c136cdf3a9ef1e5bf4aee4211
CamilaDamasio <camiladamasioef@hotmail.com>
2021-08-19 09:13:58 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
+import Provider from './context/Provider';
+import Table from './components/Table';
 
 function App() {
   return (
-    <span>Hello, App!!</span>
+    // O Provider tem que estar no App.js
+    <Provider>
+      <Table />
+    </Provider>
   );
 }
 
