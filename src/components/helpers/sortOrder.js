f22e38b9324fed4a1e5c470c58600bb9a079362e
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-08-20 09:25:02 -0300

    req 6 - Ordene as colunas de forma ascendente ou descendente

--- src/components/helpers/sortOrder.js
+++ src/components/helpers/sortOrder.js
@@ -1,5 +1,26 @@
+function sortNumber(array, filterss) {
+  const { order: { column, sort } } = filterss;
+  console.log(column, sort);
+  const newSort = array.sort(({ [column]: a }, { [column]: b }) => {
+    // let comp = 0;
+    const ONE_NEG = -1;
+    const an = parseInt(a, 10);
+    const bn = parseInt(b, 10);
+    // const an = (a[column] === 'unknown') ? ONE_NEG : parseInt(a[column], 10);
+    // const bn = (b[column] === 'unknown') ? ONE_NEG : parseInt(b[column], 10);
+
+    const comp = (an - bn);
+    return (sort === 'DESC') ? (comp * ONE_NEG) : comp;
+  });
+  console.log(newSort);
+  // if (sort === 'DESC') return [...newSort].reverse();
+  return [...newSort];
+}
+
 function sortOrder(array, filters) {
   const { order: { column, sort } } = filters;
+  // console.log(column, sort);
+  if (column !== 'name') return sortNumber(array, filters);
 
   const newSort = array.sort(({ [column]: a }, { [column]: b }) => { // src: https://www.sitepoint.com/sort-an-array-of-objects-in-javascript/
     let comp = 0;
@@ -10,10 +31,8 @@ function sortOrder(array, filters) {
     } else if (a < b) {
       comp = ONE_NEG;
     }
-
-    return (
-      (sort === 'DESC') ? (comp * ONE_NEG) : comp
-    );
+    comp = a.localeCompare(b);
+    return (sort === 'DESC') ? (comp * ONE_NEG) : comp;
   });
 
   return ([...newSort]);
