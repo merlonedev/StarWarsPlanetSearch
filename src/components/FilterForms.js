c28ebe96048bd4cb365bf0d743f8deb5c5e4a091
julio11lp <julio11lp@hotmail.com>
2021-08-21 12:55:55 -0300

    codigo quebrando na linha 21 apos usar a funcao handleNumericChange

--- src/components/FilterForms.js
+++ src/components/FilterForms.js
@@ -4,18 +4,19 @@ import MyContext from '../context/Context';
 const COLUMNS_FILTER_ARRAY = ['population',
   'orbital_period', 'diameter', 'rotation_period', 'surface_water'];
 
+const INITIAL_STATE = { filters: {
+  filterByName: { name: '' },
+  filterByNumericValues: [
+    {
+      column: 'population',
+      comparison: '',
+      value: '',
+    },
+  ],
+} };
+
 function FilterForms() {
-  const [filters, setFilters] = useState({
-    filters: {
-      filterByName: { name: '' },
-      filterByNumericValues: [
-        {
-          column: 'name',
-          comparison: '',
-          value: '',
-        },
-      ],
-    } });
+  const [filters, setFilters] = useState({ ...INITIAL_STATE });
 
   const { filters: { filterByName: { name: nameValue } } } = filters;
 
@@ -23,15 +24,24 @@ function FilterForms() {
   const { setFiltersProvider } = consumer;
 
   const handleStringChange = ({ target: { id, value, name } }) => {
-    if (id === 'filterByName') {
-      setFilters({
-        ...filters,
-        filters: {
-          [id]: {
-            [name]: value,
-          },
-        } });
-    }
+    setFilters({
+      ...filters,
+      filters: {
+        [id]: {
+          [name]: value,
+        },
+      } });
+  };
+
+  const handleNumericChange = ({ target: { value, name } }) => {
+    setFilters({
+      ...filters,
+      filters: {
+        filterByNumericValues: [{
+          [name]: value,
+        }],
+      },
+    });
   };
 
   useEffect(() => setFiltersProvider(filters));
@@ -50,12 +60,15 @@ function FilterForms() {
           placeholder="Nome do planeta"
         />
       </label>
+
       <label htmlFor="filterByNumericValues">
         Atributos
         <select
           id="filterByNumericValues"
           name="column"
           data-testid="column-filter"
+          value=""
+          onChange={ handleNumericChange }
         >
           { COLUMNS_FILTER_ARRAY.map((item) => (
             <option
