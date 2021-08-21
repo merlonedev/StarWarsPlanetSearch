37e140cc0447b4fb083f012f72701f774b3473b4
tomsantos07 <xotmsantosx@live.com>
2021-08-20 23:30:46 -0300

    renderizando componentes para realizaçao de filtragem

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Table from './components/Table';
 import Filters from './components/Filters';
+import NumericFilter from './components/NumericFilter';
 import NewProvider from './context/MyContext';
 
 function App() {
   return (
     <div>
       <NewProvider>
+        <NumericFilter />
         <Filters />
         <Table />
       </NewProvider>
---
+++ src/components/NumericFilter.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+function NumericFilter() {
+  return (
+    <div>
+      <select
+        data-testid="column-filter"
+      >
+        <option value="population">population</option>
+        <option value="orbital_period">orbital_period</option>
+        <option value="diameter">diameter</option>
+        <option value="rotation_period">rotation_period</option>
+        <option value="surface_water">surface_water</option>
+      </select>
+
+      <select
+        data-testid="comparison-filter"
+      >
+        <option value="menor que">menor que</option>
+        <option value="maior que">maior que</option>
+        <option value="igual a">igual a</option>
+      </select>
+
+      <input
+        data-testid="value-filter"
+        type="number"
+      />
+
+      <button
+        data-testid="button-filter"
+        type="button"
+      >
+        Buscar
+      </button>
+    </div>
+  );
+}
+
+export default NumericFilter;
