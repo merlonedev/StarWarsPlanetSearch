6764e7ef000a285544d152f9ebab6a1eacdb88d3
MaxAlves0035 <maxalves2@hotmail.com>
2021-08-21 14:42:41 -0300

    Requisito 01 - aprovado

--- src/Components/table.js
+++ src/Components/table.js
@@ -3,12 +3,34 @@ import { context } from '../Context/Context';
 
 function Table() {
   const { data } = useContext(context);
-  console.log(data);
-  return (
-    <div>
-      <p>hello</p>
-    </div>
-  );
+  let columns = [];
+  const indexOf = 9;
+
+  if (data.length) {
+    columns = Object.keys(data[0]);
+    columns.splice(indexOf, 1);
+    return (
+      <table border="1">
+        <thead>
+          <tr>
+            {
+              columns.map((key) => (
+                <th key={ key }>{ key }</th>
+              ))
+            }
+          </tr>
+        </thead>
+        <tbody>
+          { data.map((planet) => (
+            <tr key={ planet.name }>
+              { columns.map((column) => <td key={ column }>{ planet[column] }</td>) }
+            </tr>
+          )) }
+        </tbody>
+      </table>
+    );
+  }
+  return <p>Loading...</p>;
 }
 
 export default Table;
