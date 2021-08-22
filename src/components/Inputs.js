4a5dfe7629471fe3098306bd797c0cb635c356d6
CamilaDamasio <camiladamasioef@hotmail.com>
2021-08-22 15:50:42 -0300

    Inicia requisito 3 - cria campos de input para filtros

--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -26,6 +26,51 @@ function Inputs() {
           onChange={ handleChange }
         />
       </label>
+      <label htmlFor="filterColumn">
+        <select
+          data-testid="column-filter"
+          name="filterColumn"
+          id="filterColumn"
+          value={ name }
+          onChange={ handleChange }
+        >
+          <option value="population">population</option>
+          <option value="orbital_period">orbital_period</option>
+          <option value="diameter">diameter</option>
+          <option value="rotation_period">rotation_period</option>
+          <option value="surface_water">surface_water</option>
+        </select>
+      </label>
+      <label htmlFor="filterComparison">
+        <select
+          data-testid="comparison-filter"
+          name="filterComparison"
+          id="filterComparison"
+          value={ name }
+          onChange={ handleChange }
+        >
+          <option value="maior que">maior que</option>
+          <option value="menor que">menor que</option>
+          <option value="igual a">igual a</option>
+        </select>
+      </label>
+      <label htmlFor="filterValue">
+        <input
+          data-testid="value-filter"
+          name="filterValue"
+          type="number"
+          placeholder="Digite um número"
+          value={ name }
+          onChange={ handleChange }
+        />
+      </label>
+      <button
+        type="button"
+        data-testid="button-filter"
+        onClick={ () => (console.log('clicado')) /* handleClick */ }
+      >
+        Pesquisar
+      </button>
     </form>
   );
 }
