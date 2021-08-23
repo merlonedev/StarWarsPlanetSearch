e4e082380530c17c0d83d412c854a1dce277ae33
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-08-23 10:26:24 -0300

    remove comments

--- src/hooks/usePlanets.js
+++ src/hooks/usePlanets.js
@@ -4,18 +4,6 @@ import fetchPlanets from '../services/planetsAPI';
 function usePlanets() {
   const [data, setData] = useState({});
   const [loading, setLoading] = useState(true);
-  // const [headers, setHeaders] = useState([]);
-  // const [planetsInfo, setPlanetsInfo] = useState([]);
-
-  // function filterHeaders(planet) {
-  //   const filtered = Object.keys(planet).filter((e) => e !== 'residents');
-  //   setHeaders(filtered);
-  // }
-
-  // function removeResidentsInfo(planets) {
-  //   const filtered = planets.map((e) => delete e.residents);
-  //   return filtered;
-  // }
 
   useEffect(() => {
     async function getPlanets() {
