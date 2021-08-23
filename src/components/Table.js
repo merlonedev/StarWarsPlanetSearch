e4157c74fa33ea54dfbef196f3a9df135e184ac6
jorda <jordan.rachid@gmail.com>
2021-08-23 20:54:21 -0300

    3 sem presepada

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,22 +2,26 @@ import React, { useContext } from 'react';
 import PlanetsContext from '../context/PlanetsContext';
 
 function Table() {
-  const { data, nameSearch, numberSearch, comparison } = useContext(PlanetsContext);
+  const { data, nameSearch, numberSearch, comparison,
+    param } = useContext(PlanetsContext);
   if (!data.length) return <h1>Loading</h1>;
-  let planets = data.filter((planet) => planet.name.includes(nameSearch));
+  let planets = data;
   // let planetsFil = [];
+  if (nameSearch !== '') {
+    planets = data.filter((planet) => planet.name.includes(nameSearch));
+  }
 
   if (comparison === 'maior que') {
     planets = planets
-      .filter((planet) => parseInt(planet.diameter, 10) > numberSearch);
+      .filter((planet) => parseInt(planet[param], 10) > numberSearch);
   }
   if (comparison === 'menor que') {
     planets = planets
-      .filter((planet) => parseInt(planet.surface_water, 10) < numberSearch);
+      .filter((planet) => parseInt(planet[param], 10) < numberSearch);
   }
   if (comparison === 'igual a') {
     planets = planets
-      .filter((planet) => parseFloat(planet.population) === parseFloat(numberSearch));
+      .filter((planet) => parseFloat(planet[param]) === parseFloat(numberSearch));
     // console.log(typeof numberSearch);
     // data.filter((planet) => console.log(typeof parseFloat(planet.population)));
   }
