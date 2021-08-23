63022e654ac6c7c1d07f73de14b360b5b32f1586
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-08-23 19:16:58 -0300

    implementa chamada do componente FilterName

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import MyProvider from './provider/MyProvider';
 import Table from './components/Table';
+import FilterName from './components/FilterName';
 
 function App() {
   return (
     <MyProvider>
+      <FilterName />
       <Table />
     </MyProvider>
   );
