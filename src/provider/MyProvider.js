05f14894f177150af346be95ce99b00b805b0c35
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-08-23 17:44:44 -0300

    implementa fetch dos planetas no context

--- src/provider/MyProvider.js
+++ src/provider/MyProvider.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import MyContext from '../context/MyContext';
 
 function MyProvider({ children }) {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchPlanets = () => {
+      fetch('https://swapi-trybe.herokuapp.com/api/planets/')
+        .then((resolve) => resolve.json())
+        .then((resolve) => setData(resolve));
+    };
+    fetchPlanets();
+  }, []);
+
+  const contextValue = {
+    data,
+  };
+
   return (
-    <MyContext.Provider value={ {} }>
+    <MyContext.Provider value={ contextValue }>
       {children}
     </MyContext.Provider>
   );
