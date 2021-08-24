ca294db3aeb81e0332f2a7acd4e3bdbde396e58a
jmlinger <jmlinger10@gmail.com>
2021-08-24 18:27:55 -0300

    add referencial

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import FilterInputs from './components/FilterInputs';
 import Table from './components/Table';
 import Provider from './context/Provider';
 
+// Aplicativo desenvolvido com orientação do colega Cristiano Lima, turma 12. Perfil no github: https://github.com/cristianocsl.
 function App() {
   return (
     <Provider>
