2cfc9db688569c21f53bef618c9f85f7e59a811d
Ed Santos <edsantos2822@gmail.com>
2021-08-24 16:18:01 -0300

    Finalizando - Requisito 1 -Criando a tabela de planetas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import PlanetProvider from './context/PlanetProvider';
+import Table from './components/table/Table';
+
 import './App.css';
 
 function App() {
   return (
-    <span>Hello, App!</span>
+    <PlanetProvider>
+      <Table />
+    </PlanetProvider>
   );
 }
 
