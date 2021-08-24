477e07d240271eaeb4d4305c144861c78cfc72e0
Thaisa Medeiros <thaisa.ma@hotmail.com.br>
2021-08-24 10:39:41 -0300

    adiciona dropdown de filtro de categorias

--- src/Components/Filter.js
+++ src/Components/Filter.js
@@ -9,17 +9,28 @@ function Filter() {
   }
 
   return (
-    <label htmlFor="filterByName">
-      <input
-        type="text"
-        name="filterByName"
-        id="filterByName"
-        value={ filterByName.name }
-        onChange={ handleChange }
-        data-testid="name-filter"
-        placeholder="Pesquise aqui"
-      />
-    </label>
+    <form>
+      <label htmlFor="filterByName">
+        <input
+          type="text"
+          name="filterByName"
+          id="filterByName"
+          value={ filterByName.name }
+          onChange={ handleChange }
+          data-testid="name-filter"
+          placeholder="Pesquise aqui"
+        />
+      </label>
+      <label htmlFor="column-filter">
+        <select name="column-filter" id="column-filter" data-testid="column-filter">
+          <option value="population">population</option>
+          <option value="orbital_period">orbital_period</option>
+          <option value="diameter">diameter</option>
+          <option value="rotation_period">rotation_period</option>
+          <option value="surface_water">surface_water</option>
+        </select>
+      </label>
+    </form>
   );
 }
 
