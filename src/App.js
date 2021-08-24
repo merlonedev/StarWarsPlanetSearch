95704179b46ba10b92a16b47d31df2d515257ac1
fredmartins <fredweb22@gmail.com>
2021-08-24 20:26:17 -0300

    fechando os 80%

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import React from 'react';
 import './App.css';
 import Provider from './context/Provider';
 import Table from './components/Table';
-import FilterNAme from './components/FilterName';
+import FilterName from './components/FilterName';
 import Selectors from './components/Selectors';
 
 function App() {
   return (
-    /* REQUESITO 01 INSERINDO PROVIDER  */
     <Provider>
       <span className="title">Star Wars Planets</span>
-      <FilterNAme />
+      <FilterName />
       <Selectors />
       <Table />
     </Provider>
