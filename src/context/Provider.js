cd558846cc34bd879c6e27e2dd04700c9cff700a
felipe_louzeiro <fl.louzeiro@gmail.com>
2021-08-24 17:56:21 -0300

    feita a requisiçao

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,12 +1,22 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SWContext from './Context';
 
 function SWProvider({ children }) {
-  const context = {};
+  const [planets, setPlanets] = useState([]);
+
+  useEffect(() => {
+    const requestPlanets = async () => {
+      const api = 'https://swapi-trybe.herokuapp.com/api/planets/';
+      const response = await fetch(api);
+      const data = await response.json();
+      setPlanets(delete data.residents);
+    };
+    requestPlanets();
+  }, []);
 
   return (
-    <SWContext.Provider value={ context }>
+    <SWContext.Provider value={ SWContext }>
       {children}
     </SWContext.Provider>
   );
