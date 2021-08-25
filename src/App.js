d38de88a51ac05d5b9df0120ea5adfa4cc5f8430
Luís Seixas <luiseixas2@gmail.com>
2021-08-25 11:15:37 -0300

    faz requisicao pra API e cria tabela

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import Table from './components/Table';
+import MyProvider from './context/MyProvider';
 
 function App() {
   return (
-    <span>Star Wars Planet</span>
+    <MyProvider>
+      <Table />
+    </MyProvider>
   );
 }
 
--- src/context/MyProvider.js
+++ src/context/MyProvider.js
@@ -1,9 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import propTypes from 'prop-types';
 import MyContext from './MyContext';
 
 function Provider({ children }) {
-  const [state, setState] = useState('a');
+  const [state, setState] = useState([]);
+
+  async function fetchPlanets() {
+    const url = 'https://swapi-trybe.herokuapp.com/api/planets/';
+    await fetch(url)
+      .then((response) => response.json())
+      .then((planets) => setState(planets.results));
+  }
+
+  useEffect(() => {
+    fetchPlanets();
+  }, []);
 
   const hooks = {
     state, setState,
