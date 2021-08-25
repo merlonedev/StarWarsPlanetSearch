b1ef33b8e4aa9a3bac6c16076d910da49a95dfe2
Emerson Ovidio <emersonovidiosantos@gmail.com>
2021-08-25 20:09:18 -0300

    reinicio -R1

---
+++ src/context/Provider.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import contextApp from './contextApp';
+import searchPlanets from '../services/fetchAPI';
+
+function Provider({ children }) {
+  const [data, setData] = useState([]);
+  const contextValue = {
+    data,
+    setData,
+  };
+
+  useEffect(() => {
+    const GetPlanets = async () => {
+      const result = await searchPlanets();
+      setData(result);
+    };
+
+    GetPlanets();
+  }, []);
+
+  return (
+    <contextApp.Provider value={ contextValue }>
+      {children}
+    </contextApp.Provider>
+  );
+}
+
+Provider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Provider;
