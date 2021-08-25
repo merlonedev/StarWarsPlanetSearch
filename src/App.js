3ec536768072de2e0842ee8ff2ef44916e5ff3e0
Ed Santos <edsantos2822@gmail.com>
2021-08-25 03:36:41 -0300

    Finalizando - Requisito 2 - Criando filtro de planetas por nome

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import PlanetProvider from './context/PlanetProvider';
 import Table from './components/table/Table';
+import Input from './components/input/Input';
 
 import './App.css';
 
 function App() {
   return (
     <PlanetProvider>
+      <Input />
       <Table />
     </PlanetProvider>
   );
