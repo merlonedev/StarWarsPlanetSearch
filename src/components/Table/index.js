3264f136078fe426081277aecc1e5f89d07c4f69
Ana Laura <analauraalves25@gmail.com>
2021-08-26 00:38:23 -0300

    req 02 e 01 - resolve problemas

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -9,29 +9,25 @@ function Table() {
     <table>
       <thead>
         <tr>
-          <td>Nome</td>
-          <td>Período de Rotação</td>
-          <td>Período de Orbita</td>
-          <td>Diametro</td>
-          <td>Clima</td>
-          <td>Gravidade</td>
-          <td>Terreno</td>
-          <td>Superfície Aquática</td>
-          <td>População</td>
-          <td>Filmes</td>
-          <td>Criado</td>
-          <td>Editado</td>
-          <td>URL</td>
+          <th>Nome</th>
+          <th>Período de Rotação</th>
+          <th>Período de Orbita</th>
+          <th>Diametro</th>
+          <th>Clima</th>
+          <th>Gravidade</th>
+          <th>Terreno</th>
+          <th>Superfície Aquática</th>
+          <th>População</th>
+          <th>Filmes</th>
+          <th>Criado</th>
+          <th>Editado</th>
+          <th>URL</th>
         </tr>
       </thead>
       <tbody>
         { data.map((planets, index) => (
-          filterByName === '' ? <Planets key={ index } planetInfo={ planets } /> : null
-        )) }
-        { data.map((planets, index) => (
           planets.name.toUpperCase().includes(filterByName.toUpperCase())
-            ? <Planets key={ index } planetInfo={ planets } /> : null
-        )) }
+            ? <Planets key={ index } planetInfo={ planets } /> : null)) }
       </tbody>
     </table>
   );
