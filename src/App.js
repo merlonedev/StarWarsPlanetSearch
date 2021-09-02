9ea7736fe5bd2b6977e1c71826acd0923c720e74
johnpligeiro <johnpligeiro@gmail.com>
2021-09-02 01:26:54 -0300

    fix: move provider to App.js - req 1 ok

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Table from './components/Table';
 import './App.css';
+import Provider from './provider/Provider';
 
 function App() {
   return (
-    <Table />
+    <Provider>
+      <Table />
+    </Provider>
   );
 }
 
