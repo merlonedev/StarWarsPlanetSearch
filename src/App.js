e97e6dd5a468068f9eb0d2551adec247ccb2dc8a
LucasPeixoto <peixotolucas516@gmail.com>
2021-09-02 13:05:41 -0300

    req3 filter by value implemented

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import FilterByName from './components/FilterByName';
 import Provider from './Context/Provider';
 import Table from './components/Table';
+import FilterByValues from './components/FilterByValue';
 
 function App() {
   return (
     <div>
       <Provider>
         <FilterByName />
+        <FilterByValues />
         <Table />
       </Provider>
 
