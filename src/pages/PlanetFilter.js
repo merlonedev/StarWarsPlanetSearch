9d439b0043d7d528019abe15543e662ad857611f
Gletison Ramos da Silva Junior <gletisonjr@gmail.com>
2021-09-06 20:09:33 -0300

    Adiciona Requisito 2

--- src/pages/PlanetFilter.js
+++ src/pages/PlanetFilter.js
@@ -1,33 +1,52 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import PlanetsContext from '../context/PlanetsContext';
 import * as API from '../service/StarWarsAPI';
-// import Input from './components/Input';
+import Input from './components/Input';
+
+function filterByName(name, planets, setPlanets) {
+  setPlanets(planets.filter((element) => element.name.toLowerCase().includes(name)));
+}
 
 export default function PlanetFilter() {
   const { planets, setPlanets } = useContext(PlanetsContext);
+  const [filterName, setFilterName] = useState('');
+  const [allPlanets, setAllPlanets] = useState([]);
   const fillPlanets = async () => {
-    const myPlanets = await API.getAllPlanets();
+    const myPlanets = await API.getPlanetsFirstPage();
     setPlanets(myPlanets);
+    // myPlanets = await API.getAllPlanets();
+    setAllPlanets(myPlanets);
   };
   useEffect(() => {
     if (planets.length <= 0) {
       fillPlanets();
     }
   });
+  useEffect(() => {
+    if (filterName) {
+      filterByName(filterName, allPlanets, setPlanets);
+    }
+    if (!filterName && allPlanets.length > 0) {
+      setPlanets(allPlanets);
+    }
+  }, [filterName, allPlanets, setPlanets]);
 
-  // const [filterName, setFilterName] = useState('');
+  const handleChange = (event) => {
+    setFilterName(event.target.value);
+  };
 
   return (
     <section>
       <header>
         <h1>Header</h1>
-        {/* <Input
-            testId="email-input"
-            name="email"
-            placeholder="Email"
-            value={ userInfo.email }
-            handleChange={ handleChange }
-          /> */}
+        <Input
+          testId="name-filter"
+          text="Nome:"
+          name="filterName"
+          placeholder="Planet Name"
+          value={ filterName }
+          handleChange={ handleChange }
+        />
       </header>
     </section>
   );
--- src/service/StarWarsAPI.js
+++ src/service/StarWarsAPI.js
@@ -1,12 +1,30 @@
+async function getNext(ENDPOINT) {
+  const result = [];
+  let response = await fetch(ENDPOINT).then((data) => data.json());
+  response.results.forEach((element) => result.push(element));
+  if (response.next !== null) {
+    response = await getNext(response.next);
+    response.forEach((element) => result.push(element));
+  }
+  return result;
+}
+
 export async function getAllPlanets() {
   const ENDPOINT = 'https://swapi-trybe.herokuapp.com/api/planets/';
+  const response = await getNext(ENDPOINT);
+  console.log(response);
+  return response;
+}
+
+export async function getPlanet(id) {
+  const ENDPOINT = `https://swapi-trybe.herokuapp.com/api/planets/${id}/`;
 
   const response = await fetch(ENDPOINT).then((data) => data.json());
   return response.results;
 }
 
-export async function getPlanet(id) {
-  const ENDPOINT = `https://swapi-trybe.herokuapp.com/api/planets/${id}/`;
+export async function getPlanetsFirstPage() {
+  const ENDPOINT = 'https://swapi-trybe.herokuapp.com/api/planets/';
 
   const response = await fetch(ENDPOINT).then((data) => data.json());
   return response.results;
