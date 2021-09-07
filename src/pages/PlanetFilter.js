a213160a4e9b3c945485369b67a6f4df8d9e628b
Gletison Ramos da Silva Junior <gletisonjr@gmail.com>
2021-09-06 22:04:43 -0300

    requisito 3 incompleto

--- src/pages/PlanetFilter.js
+++ src/pages/PlanetFilter.js
@@ -7,10 +7,103 @@ function filterByName(name, planets, setPlanets) {
   setPlanets(planets.filter((element) => element.name.toLowerCase().includes(name)));
 }
 
+function getInputArray(guide) {
+  return [
+    {
+      handleChange: guide.handleChange,
+      name: 'filterName',
+      text: 'Nome:',
+      type: 'text',
+      testId: 'name-filter',
+      value: guide.filterName,
+      placeholder: 'Planet Name',
+    },
+    {
+      handleChange: guide.handleChange,
+      name: 'valor',
+      text: 'Valor:',
+      type: 'number',
+      testId: 'value-filter',
+      value: guide.filterNumber.value,
+      placeholder: 'Ex: 5000',
+    },
+  ];
+}
+
+function getSelectColumnOptions() {
+  return [
+    'population',
+    'orbital_period',
+    'diameter',
+    'rotation_period',
+    'surface_water',
+  ];
+}
+
+function getSelectCompatisonOptions() {
+  return [
+    'maior que',
+    'menor que',
+    'igual a',
+  ];
+}
+
+function getInput({
+  handleChange = null,
+  name = '',
+  text = '',
+  type = 'text',
+  testId = 'none',
+  value = null,
+  placeholder = '',
+}) {
+  return (
+    <Input
+      testId={ testId }
+      text={ text }
+      name={ name }
+      type={ type }
+      placeholder={ placeholder }
+      value={ value }
+      handleChange={ handleChange }
+    />
+  );
+}
+
+function getSelect({
+  handleChange = null,
+  name = '',
+  testId = 'none',
+  optionList = null,
+}) {
+  return (
+    <select
+      data-testid={ testId }
+      onChange={ handleChange }
+      name={ name }
+    >
+      <option value="">All</option>
+      { optionList.map((myOption, index) => (
+        <option
+          key={ index }
+          value={ myOption }
+        >
+          { myOption }
+        </option>
+      ))}
+    </select>
+  );
+}
+
 export default function PlanetFilter() {
   const { planets, setPlanets } = useContext(PlanetsContext);
   const [filterName, setFilterName] = useState('');
   const [allPlanets, setAllPlanets] = useState([]);
+  const [filterNumber, setfilterNumber] = useState({
+    column: '',
+    comparison: '',
+    value: 0,
+  });
   const fillPlanets = async () => {
     const myPlanets = await API.getPlanetsFirstPage();
     setPlanets(myPlanets);
@@ -32,21 +125,39 @@ export default function PlanetFilter() {
   }, [filterName, allPlanets, setPlanets]);
 
   const handleChange = (event) => {
-    setFilterName(event.target.value);
+    const { name, value } = event.target;
+    switch (name) {
+    case 'filterName':
+      setFilterName(value);
+      break;
+    case 'column':
+      setfilterNumber((prevState) => (
+        { ...prevState, column: value }
+      ));
+      break;
+    case 'comparison':
+      setfilterNumber((prevState) => (
+        { ...prevState, comparison: value }
+      ));
+      break;
+    case 'value':
+      setfilterNumber((prevState) => (
+        { ...prevState, value }
+      ));
+      break;
+    default:
+    }
+    console.log(`Nome:${name}, Valor:${value}`);
   };
 
   return (
     <section>
       <header>
         <h1>Header</h1>
-        <Input
-          testId="name-filter"
-          text="Nome:"
-          name="filterName"
-          placeholder="Planet Name"
-          value={ filterName }
-          handleChange={ handleChange }
-        />
+        { getInput(getInputArray({ handleChange, filterName, filterNumber })[0]) }
+        <section>
+          { getSelect({ handleChange, testId: 'column-filter', name: 'column', optionList: getSelectColumnOptions()})}
+        </section>
       </header>
     </section>
   );
