37cb33d5e5a18b7bf2b16a28c600e8d68d065afb
teofilobrandao <teofiloafssbrandao@gmail.com>
2021-09-07 14:17:05 -0300

    Projeto Starwars Planet Search - Envio Requisitos 1 e 2

--- src/components/GetPlanet.js
+++ src/components/GetPlanet.js
@@ -1,16 +1,47 @@
-import { useEffect, useState } from 'react';
-import Table from '../components/Table';
+// Resolução com o apoio do colega Nikolas Silva
+// https://www.youtube.com/watch?v=k0cZA0NYTyQ
+// https://www.youtube.com/watch?v=5Tq4-UgPTDs
+
+import React, { useEffect, useState } from 'react';
+import Table from './Table';
 import { useProvider } from '../context/Provider';
 
 function GetPlanet() {
   const [data, setData] = useState([]);
   const { filters, setFilters } = useProvider();
-}
 
-useEffect(() => {
-  fetch('https://swapi-trybe.herokuapp.com/api/planets/')
-    .then((response) => response.json())
-    .then((json) => setData(json.results));
-}, []);
+  useEffect(() => {
+    fetch('https://swapi-trybe.herokuapp.com/api/planets/')
+      .then((response) => response.json())
+      .then((json) => setData(json.results));
+  }, []);
+
+  function filterData(rows) {
+    const { filterByName: { name } } = filters;
+    const minusOne = -1;
+    return rows.filter((row) => (
+      row.name.toLowerCase().indexOf(name.toLowerCase()) > minusOne
+    ));
+  }
+
+  return (
+    <div>
+      <label htmlFor="name-filter">
+        Pesquisar Planeta:
+        <input
+          type="text"
+          id="name-filter"
+          data-testid="name-filter"
+          onChange={ (e) => setFilters({
+            filterByName: {
+              name: e.target.value,
+            },
+          }) }
+        />
+      </label>
+      <Table dataTable={ filterData(data) } />
+    </div>
+  );
+}
 
 export default GetPlanet;
