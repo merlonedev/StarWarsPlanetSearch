2b80a1b810fd4003010b32600efce9358e5c63a3
Laryssa Martins <laryssamartinsoffice@gmail.com>
2021-09-08 20:02:56 -0300

    Cria componente pra tag select

---
+++ src/Components/componentsHeader/Selector.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { string, func, arrayOf } from 'prop-types';
+
+function Selector(props) {
+  const { testid, handleChange, value, options, name } = props;
+
+  return (
+    <select
+      data-testid={ testid }
+      name={ name }
+      value={ value }
+      onChange={ handleChange }
+    >
+      {
+        options
+          .map(
+            (item, index) => <option key={ index }>{ item }</option>,
+          )
+      }
+    </select>
+  );
+}
+
+Selector.propTypes = {
+  testid: string.isRequired,
+  handleChange: func.isRequired,
+  value: string.isRequired,
+  options: arrayOf(string).isRequired,
+  name: string.isRequired,
+};
+
+export default Selector;
