4b6a6ee822c833759430b99028a51ff2812dbc46
Laryssa Martins <laryssamartinsoffice@gmail.com>
2021-09-07 23:40:33 -0300

    implementa campo de busca por nome

--- src/Components/componentsHeader/Header.js
+++ src/Components/componentsHeader/Header.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import MyContext from '../../Context/MyContext';
 
 function Header() {
+  const { filters, setFilters } = useContext(MyContext);
+
   return (
     <header>
-      <h1> Filtros </h1>
+      <h1> Star Wars Planets </h1>
+      <div>
+        <label htmlFor="filterByName">
+          Filter By Name:
+          <input
+            id="filterByName"
+            data-testid="name-filter"
+            onChange={ ({ target: { value } }) => setFilters({
+              ...filters,
+              filterByName: {
+                name: value,
+              },
+            }) }
+          />
+        </label>
+      </div>
     </header>
   );
 }
