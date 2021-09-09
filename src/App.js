1e1c759d39862356fe0b444fff5aab4758588b17
Thiago da Paz <thiagopazjunior@gmail.com>
2021-09-09 10:28:28 -0300

    Finalizando projeto.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import './App.css';
+import PlanetProvider from './Context/PlanetProvider';
+import Table from './Components/Table';
+import ActiveFilters from './Components/ActiveFilters';
+import FiltersForm from './Components/FiltersForm';
 
 function App() {
   return (
-    <span>Hello, App!</span>
+    <div>
+      <PlanetProvider>
+        <FiltersForm />
+        <ActiveFilters />
+        <Table />
+      </PlanetProvider>
+    </div>
   );
 }
 
