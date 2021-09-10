7f3932023e17aebdb65d3b82b24664c10fd31647
camila-mp <ca.malvp@gmail.com>
2021-09-10 10:40:40 -0300

    corrige style lint e adiciona estilos em UsedFiltersDisplay

--- src/Components/UsedFiltersDisplay.js
+++ src/Components/UsedFiltersDisplay.js
@@ -20,12 +20,13 @@ function UsedFiltersDisplay() {
   return (
     <div className="used-filters-div">
       { filterByNumericValues.map(({ comparison, value, column }) => (
-        <div key={ column } data-testid="filter">
+        <div key={ column } data-testid="filter" className="filter-display">
           { column }
           { ' ' }
           { comparison }
           { ' ' }
           { value }
+          { ' ' }
           <button
             type="button"
             onClick={ () => handleClick(column) }
