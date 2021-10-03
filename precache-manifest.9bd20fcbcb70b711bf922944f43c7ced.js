5b65a41d030d0658a6d1048865d62904e2aa747a
merlone <alestori7@gmail.com>
2021-10-03 11:34:22 -0300

    Updates

---
+++ precache-manifest.9bd20fcbcb70b711bf922944f43c7ced.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "041c71d1ce8b11e30d8180ebbe650234",
+    "url": "/StarWarsPlanetSearch/index.html"
+  },
+  {
+    "revision": "07dc263118916398d56b",
+    "url": "/StarWarsPlanetSearch/static/css/main.32174912.chunk.css"
+  },
+  {
+    "revision": "0f6d8f9cbe4d2b6132e7",
+    "url": "/StarWarsPlanetSearch/static/js/2.bf734956.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/StarWarsPlanetSearch/static/js/2.bf734956.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07dc263118916398d56b",
+    "url": "/StarWarsPlanetSearch/static/js/main.1dd564a0.chunk.js"
+  },
+  {
+    "revision": "f0042a414c505facc7f6",
+    "url": "/StarWarsPlanetSearch/static/js/runtime-main.a5d4a132.js"
+  },
+  {
+    "revision": "dcb5cf864bdbede594c7d31a204dee8d",
+    "url": "/StarWarsPlanetSearch/static/media/StarJediHollow-A4lL.dcb5cf86.ttf"
+  },
+  {
+    "revision": "a11b0a3c0f54aabc113878791da936ef",
+    "url": "/StarWarsPlanetSearch/static/media/anewhope.a11b0a3c.jpeg"
+  },
+  {
+    "revision": "739e58c0994506b60d2176f538cf75c1",
+    "url": "/StarWarsPlanetSearch/static/media/star-wars-logo-974.739e58c0.png"
+  }
+]);
